c3a4e74affe10baed1c970b3f990285521a8ed02
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-03-01 10:04:41 +0700

    improved factorial

--- src/main/java/algorithm/Factorial.java
+++ src/main/java/algorithm/Factorial.java
@@ -1,16 +1,37 @@
 package algorithm;
 
+import java.util.List;
+
 public class Factorial {
     public static void main(String[] args) {
         // 4! = 24
         // 5! = 120;
-        System.out.println("Factorial of 4 is: " + factorial(4));
-        System.out.println("Factorial of 4 is: " + factorial(5));
+        List<Integer> numbers = List.of(4, 5, 6, 7, 8, 9, 10);
+        numbers.forEach(Factorial::factorialWithMessage);
+    }
+
+    private static void factorialWithMessage(int number) {
+        String result = message(number) + " is " + factorial(number);
+        System.out.println(result);
     }
 
     private static int factorial(int number) {
         if (number == 1) return 1;
 
         return number * factorial(number - 1);
+    }
+
+    private static String message(int number) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Factorial of ").append(number).append("! (");
+        for (int i = number; i > 0; i--) {
+            if (i == 1) {
+                stringBuilder.append(i);
+            } else {
+                stringBuilder.append(i).append(" x ");
+            }
+        }
+        stringBuilder.append(")");
+        return stringBuilder.toString();
     }
 }
